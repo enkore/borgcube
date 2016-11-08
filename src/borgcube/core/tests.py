8b727cd92cfc1050ee8c762d2ed2ca8b0b05f543
Marian Beermann <public@enkore.de>
2016-11-08 17:08:59 +0100

    Add some core model tests
    Low-hanging fruits and all

--- src/borgcube/core/tests.py
+++ src/borgcube/core/tests.py
@@ -47,3 +47,61 @@ def job(client, repository):
         repository=repository,
         client=client,
     )
+
+
+def test_job_state(job):
+    assert job.state == Job.State.job_created
+    assert job.db_state == 'job-created'
+    assert not job.failed
+
+
+def test_job_update_state(job):
+    assert job.state == Job.State.job_created
+    job.update_state(Job.State.job_created, Job.State.failed)
+    assert job.state == Job.State.failed
+    assert job.failed
+
+
+def test_job_update_state_str(job):
+    with pytest.raises(Exception):
+        job.update_state(job.state, 'something')
+    with pytest.raises(Exception):
+        job.update_state('something', job.state)
+
+
+def test_job_update_state_fail(job):
+    with pytest.raises(ValueError):
+        job.update_state(Job.State.client_done, Job.State.failed)
+    assert job.state == Job.State.job_created
+
+
+def test_job_force_state(job):
+    assert not job.force_state(job.state)
+    assert job.force_state(Job.State.failed)
+    assert job.state == Job.State.failed
+
+
+def test_job_archive_name(job):
+    assert job.archive_name == 'testhost-%s' % job.id
+    assert 'UUID' not in job.archive_name
+
+
+def test_job_data(job):
+    data = job.data
+    data['key'] = True
+    list = data.setdefault('list', [])
+    list.append('string')
+    job.save()
+    job.refresh_from_db()
+    assert job.data['key'] is True
+    assert job.data['list'] == ['string']
+
+
+def test_job_data_refs_int_keys(job):
+    job.data['d'] = {}
+    job.data['d']['a'] = 1
+    job.data['d'].update({1: 2})
+    job.save()
+    job.refresh_from_db()
+    # Take careful note: JSON keys are always strings. non-str keys are converted to a string.
+    assert job.data['d'] == {'a': 1, '1': 2}
