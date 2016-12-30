22ffc18d901856c14c5d02911a2aaeb73285fff0
Marian Beermann <public@enkore.de>
2016-12-30 22:23:37 +0100

    delete reldb config

--- src/borgcube/web/settings.py
+++ src/borgcube/web/settings.py
@@ -82,18 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'borgcube.web.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(Path(__file__).parent.parent.parent.parent / 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
